4de6fe43856789d03a8a698ce1f3ff2626f732f1
BrandonFields <brandonefields@gmail.com>
2021-03-29 15:38:24 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function introductionWithLanguage(name, language) {
 
 function introductionWithLanguageOptional(name, language = "JavaScript") {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-    return ` Hi, my name is ${Gracie} and I am learning to program in ${Python}.`;
+
 
   }
 
